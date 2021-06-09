d6256af3c2645e6d454f3e677bb26c36fc381652
Fede <fede.fede.website>
2021-06-09 14:31:23 -0500

    jason with webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import style from "./style.css";
 import logo from "./assets/webpack.svg";
 import gemini from "./assets/saga-kanon.png";
+import data from "./data.json";
 
 const arr = [1, 2, 3],
  codeESNext = () => console.log(...arr);
@@ -17,13 +18,21 @@ const d = document,
  $app = d.getElementById("app"),
  $h1 = d.createElement("h1"),
  $logo = d.createElement("img"),
- $img = d.createElement("img");
+ $img = d.createElement("img"),
+ $nav = d.createElement("nav");
+
+let menu = "";
+
+data.links.forEach((el) => (menu += `<a href="${el[1]}">${el[0]}</a>`)); 
  
 $h1.textContent = "Hola mundo con Webpack";
 $logo.src = logo;
 $logo.classList.add("icon");
 $img.src = gemini;
+$nav.innerHTML = menu;
+$nav.classList.add("menu");
 
-$app.appendChild($h1)
-$app.appendChild($logo)
-$app.appendChild($img)
+$app.appendChild($h1);
+$app.appendChild($logo);
+$app.appendChild($nav);
+$app.appendChild($img);
