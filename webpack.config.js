3b3cfa05690692d24ad4189af0c13b209be17a17
Fede <fede.fede.website>
2021-06-17 17:52:02 -0500

    React with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test:/\.js$/i,
+                test:/\.jsx?$/i,
                 exclude:/node_modules/,
                 use: {
                     loader:"babel-loader"
